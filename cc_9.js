6180b8ee6bdf8297fecffcc0338035ae76bc26d0
madisynnotz <madisynnotz33@gmail.com>
2025-02-23 16:01:54 -0500

    Update cc_9.js

--- cc_9.js
+++ cc_9.js
@@ -1,6 +1,6 @@
 // Task 1: Creating an Employee Class
 
-class Employee {
+class Employee { // create employee class
     constructor(name, id, department, salary) {
       this.name = name;
       this.id = id;
@@ -8,11 +8,11 @@ class Employee {
       this.salary = salary;
     }
   
-    getDetails() {
+    getDetails() { // creating getdetails
       return `Employee: ${this.name}, ID: ${this.id}, Department: ${this.department}, Salary: $${this.salary.toLocaleString()}`;
     }
   
-    calculateAnnualSalary() {
+    calculateAnnualSalary() { // creating calculateannualsalary
       return this.salary * 12;
     }
   }
@@ -20,7 +20,7 @@ class Employee {
   // Example Usage
   const emp1 = new Employee("Alice Johnson", 101, "Sales", 5000);
   console.log(emp1.getDetails());
-  console.log(`Annual Salary: $${emp1.calculateAnnualSalary().toLocaleString()}`);
+  console.log(emp1.calculateAnnualSalary())`);
 
   // Task 2: Creating a Manager Class (Inheritance & Method Overriding)
 
@@ -42,17 +42,17 @@ class Employee {
       }
     }
     
-    class Manager extends Employee {
+    class Manager extends Employee { // create manager with extending employee
       constructor(name, id, department, salary, teamSize) {
         super(name, id, department, salary);
-        this.teamSize = teamSize;
+        this.teamSize = teamSize; // adding team size
       }
   
-      getDetails() {
+      getDetails() { // override
         return `Manager: ${this.name}, ID: ${this.id}, Department: ${this.department}, Salary: $${this.salary.toLocaleString()}, Team Size: ${this.teamSize}`;
       }
   
-      calculateBonus() {
+      calculateBonus() { // adding calculatebonus
         return this.salary * 12 * 0.1;
       }
     }
@@ -69,17 +69,17 @@ class Employee {
 
   // Task 3: Creating a Company Class
 
-  class Company {
-    constructor(name) {
+  class Company { // new class company
+    constructor(name) { // set up
         this.name = name;
-        this.employees = [];
+        this.employees = []; // initialize
     }
     
-    addEmployee(employee) {
+    addEmployee(employee) { // set up add employee
         this.employees.push(employee);
     }
     
-    listEmployees() {
+    listEmployees() { // set up list employee
         this.employees.forEach(employee => console.log(employee.getDetails()));
     }
 }
@@ -91,7 +91,7 @@ company.listEmployees();
 
 // Task 4: Implementing a Payroll System
 
-Company.prototype.calculateTotalPayroll = function() {
+Company.prototype.calculateTotalPayroll = function() { // add calculatetotalpayroll
     return this.employees.reduce((total, employee) => {
         if (employee instanceof Manager) {
             return total + employee.calculateAnnualSalary() + employee.calculateBonus();
@@ -104,7 +104,7 @@ console.log(company.calculateTotalPayroll());
 
 // Task 5: Implementing Promotions
 
-Company.prototype.promoteToManager = function(employee, teamSize) {
+Company.prototype.promoteToManager = function(employee, teamSize) { 
     const index = this.employees.indexOf(employee);
     if (index !== -1) {
         const newManager = new Manager(employee.name, employee.id, employee.department, employee.salary, teamSize);
